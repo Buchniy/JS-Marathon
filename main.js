4b75fd16931e6fb9aa7f32e9f38c52472fbfe841
Бучный Александр Николаевич <buchniy@technoton.local>
2020-09-24 13:14:42 +0300

    Home work 2

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log('hello');
+const firstRow = 'мама мыла раму';
+const secondRow = 'собака друг человека';
+
+function getRow(first, second) {
+
+    if (searchLetterA(first) > searchLetterA(second)) {
+        console.log(first);
+    }else if(searchLetterA(first) === searchLetterA(second)){
+        console.log(first + ', ' + second);
+    } else {
+        console.log(second);
+    }
+}
+
+function searchLetterA(line) {
+    let arr = line.split('');
+    let a = null;
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i] === 'а') {
+            a += 1;
+        }
+    }
+    return a;
+}
+
+getRow(firstRow, secondRow); // мама мыла раму
